3ff621ab24e13e3861c170864a55f059773d4f76
Leonardo Perez <happyleonardo77@gmail.com>
2026-02-10 06:55:24 -0500

    update deploy.yml

--- next.config.js
+++ next.config.js
@@ -1,8 +1,40 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',
+//   assetPrefix: '',
+//   basePath: '',
+// };
 const nextConfig = {
   output: 'export',
-  assetPrefix: '',
-  basePath: '',
-};
+  images: {
+    unoptimized: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+
+    // Workaround for undici private fields
+    config.module.rules.push({
+      test: /\.js$/,
+      include: /node_modules\/undici/,
+      use: {
+        loader: 'swc-loader',
+      },
+    });
+
+    return config;
+  },
+  experimental: {
+    // This helps with Firebase compatibility
+    esmExternals: 'loose',
+  },
+}
+
 
 module.exports = nextConfig;
